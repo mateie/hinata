79b2e21e1287aea8f66707ad4988f60874681400
mateie <matemachitidze2001@gmail.com>
2020-07-29 15:59:45 -0700

    Fixed Bugs

--- dashboard/app.js
+++ dashboard/app.js
@@ -267,7 +267,7 @@ exports.lightOrDark = color => {
     }
 }
 
-exports.secondsToDuration = sec {
+exports.secondsToDuration = sec => {
     let hours = Math.floor(sec / 3600);
     let minutes = Math.floor((sec - (hours * 3600)) / 60);
     let seconds = sec - (hours * 3600) - (minutes * 60);
