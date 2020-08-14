a04dde9f6765ff80435ad10a85ca7894092ffe51
mateie <matemachitidze2001@gmail.com>
2020-08-14 01:10:22 -0700

    Kicking users and banning them are enabled (still buggy)

--- dashboard/routes/members.js
+++ dashboard/routes/members.js
@@ -3,12 +3,12 @@ const Main = require('../app');
 const { client } = require('../../index');
 const Discord = require('discord.js');
 
-members.get('/:userID', async (req, res) => {
+members.get('/:memberID', async (req, res) => {
     const guild = client.guilds.cache.get(req.guildID);
     if (!guild) {
         return res.redirect('/');
     }
-    const member = guild.members.cache.get(req.params.userID);
+    const member = guild.members.cache.get(req.params.memberID);
     if (!member) {
         return res.redirect('/');
     }
@@ -72,27 +72,61 @@ members.get('/:userID', async (req, res) => {
     Main.renderTemplate(res, req, 'user.ejs', { perms: Discord.Permissions, capL: Main.capFirstLetter, capA: Main.capAllLetters, member: member, status: memberStatus, color: bgColor, activity: activity, brightness: brightness });
 });
 
-members.post('/:userID', async (req, res) => {
+members.post('/:memberID', async (req, res) => {
     const guild = client.guilds.cache.get(req.guildID);
-    if(!guild) {
+    if (!guild) {
         return res.redirect('/');
     }
-    const member = guild.members.cache.get(req.params.userID);
-    if(!member) {
+    const member = guild.members.cache.get(req.params.memberID);
+    if (!member) {
         return res.redirect('/');
     }
 
-    console.log(req.body);
-
-    if(req.body.memberNickname.length > 0) {
+    if (req.body.memberNickname.length > 0) {
         member.setNickname(req.body.memberNickname);
     }
 
-    if(req.body.clearNickname.length === 0) {
+    if (req.body.clearNickname.length === 0) {
         member.setNickname('');
     }
 
     res.redirect(req.originalUrl);
 });
 
-module.exports = users;
+members.post('/:memberID/kick', async (req, res) => { 
+    const guild = client.guilds.cache.get(req.guildID);
+    if (!guild) {
+        return res.redirect('/');
+    }
+    const member = guild.members.cache.get(req.params.memberID);
+    if (!member) {
+        return res.redirect('/');
+    }
+
+    member.kick().then(() => {
+        res.send('Kicked User');
+    })
+    .catch(err => {
+        res.send(`Error: ${err}`);
+    });
+});
+
+members.post('/:memberID/ban', async (req, res) => { 
+    const guild = client.guilds.cache.get(req.guildID);
+    if (!guild) {
+        return res.redirect('/');
+    }
+    const member = guild.members.cache.get(req.params.memberID);
+    if (!member) {
+        return res.redirect('/');
+    }
+
+    member.ban().then(() => {
+        res.send('Banned User');
+    })
+    .catch(err => {
+        res.send(`Error: ${err}`);
+    });
+});
+
+module.exports = members;
