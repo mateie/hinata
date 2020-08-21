1d62c437bc5ec718578a7592367e003310e58b52
mateie <matemachitidze2001@gmail.com>
2020-08-20 22:26:00 -0700

    Added Members page

--- dashboard/routes/members.js
+++ dashboard/routes/members.js
@@ -28,8 +28,8 @@ members.get('/:memberID', async (req, res) => {
 
     if (!memberActivity || memberActivity.length < 1) {
         activity = {
-            name: 'Nothing',
-            type: 'Doing',
+            name: '',
+            type: 'None',
         };
     } else if (memberActivity.type === 'CUSTOM_STATUS') {
         activity = {
@@ -69,64 +69,7 @@ members.get('/:memberID', async (req, res) => {
 
     let brightness = Main.lightOrDark(bgColor) ? 'text-dark' : 'text-light';
 
-    Main.renderTemplate(res, req, 'member.ejs', { perms: Discord.Permissions, capL: Main.capFirstLetter, capA: Main.capAllLetters, member: member, status: memberStatus, color: bgColor, activity: activity, brightness: brightness });
-});
-
-members.post('/:memberID', async (req, res) => {
-    const guild = client.guilds.cache.get(req.guildID);
-    if (!guild) {
-        return res.redirect('/');
-    }
-    const member = guild.members.cache.get(req.params.memberID);
-    if (!member) {
-        return res.redirect('/');
-    }
-
-    if (req.body.memberNickname.length > 0) {
-        member.setNickname(req.body.memberNickname);
-    }
-
-    if (req.body.clearNickname.length === 0) {
-        member.setNickname('');
-    }
-
-    res.redirect(req.originalUrl);
-});
-
-members.post('/:memberID/kick', async (req, res) => { 
-    const guild = client.guilds.cache.get(req.guildID);
-    if (!guild) {
-        return res.redirect('/');
-    }
-    const member = guild.members.cache.get(req.params.memberID);
-    if (!member) {
-        return res.redirect('/');
-    }
-
-    member.kick().then(() => {
-        res.send('Kicked User');
-    })
-    .catch(err => {
-        res.send(`Error: ${err}`);
-    });
-});
-
-members.post('/:memberID/ban', async (req, res) => { 
-    const guild = client.guilds.cache.get(req.guildID);
-    if (!guild) {
-        return res.redirect('/');
-    }
-    const member = guild.members.cache.get(req.params.memberID);
-    if (!member) {
-        return res.redirect('/');
-    }
-
-    member.ban().then(() => {
-        res.send('Banned User');
-    })
-    .catch(err => {
-        res.send(`Error: ${err}`);
-    });
+    Main.renderTemplate(res, req, 'member.ejs', { perms: Discord.Permissions, capL: Main.capFirstLetter, capA: Main.capAllLetters, member: member, status: memberStatus, color: bgColor, activity: activity, ownerID: process.env.OWNER_ID });
 });
 
 module.exports = members;
